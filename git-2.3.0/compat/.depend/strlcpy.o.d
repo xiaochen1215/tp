75426f3a03070e88df961c86b73b085b0d53c254
xiaochen <905203595@qq.com>
2019-12-23 04:51:08 +0800

    项目

--- git-2.3.0/compat/.depend/strlcpy.o.d
+++
@@ -1,8 +0,0 @@
-compat/strlcpy.o: compat/strlcpy.c compat/../git-compat-util.h \
- compat/../compat/bswap.h compat/../wildmatch.h
-
-compat/../git-compat-util.h:
-
-compat/../compat/bswap.h:
-
-compat/../wildmatch.h:
